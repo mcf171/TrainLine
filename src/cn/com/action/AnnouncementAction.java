3eb6a03e346f5871c4bd5445ec76ba391d90d9dc
mcf171 <453372939@qq.com>
2013-10-10 23:18:26 +0800

    the thirth commit

--- src/cn/com/action/AnnouncementAction.java
+++ src/cn/com/action/AnnouncementAction.java
@@ -3,5 +3,8 @@ package cn.com.action;
 import cn.com.base.BaseActionSupport;
 
 public class AnnouncementAction extends BaseActionSupport{
-public void test(){}
+public void test(){
+	
+	System.out.println("test");
+}
 }
