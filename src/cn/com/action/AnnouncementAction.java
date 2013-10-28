1bccf018935507035d0adf856081901947a7fe35
zongyulang <zongyulang@zongyulang-PC.mshome.net>
2013-10-28 22:27:26 +0800

    xuetai

--- src/cn/com/action/AnnouncementAction.java
+++ src/cn/com/action/AnnouncementAction.java
@@ -1,7 +1,11 @@
 package cn.com.action;
 
 import cn.com.base.BaseActionSupport;
-
+/**
+ * 
+ * @author zongyulang
+ *
+ */
 public class AnnouncementAction extends BaseActionSupport{
 public void test(){
 	
