73311c694b8a468a7bf7c70c068e189fdc0fc5d7
zongyulang <zongyulang@zongyulang-PC.mshome.net>
2013-10-29 20:37:30 +0800

    xuetai 注释修改

--- src/cn/com/action/AnnouncementAction.java
+++ src/cn/com/action/AnnouncementAction.java
@@ -2,19 +2,7 @@ package cn.com.action;
 
 import cn.com.base.BaseActionSupport;
 /**
-<<<<<<< HEAD
- * madegecao
- * @author zongyulang
-=======
- * Test branch 1
- * @author MCF
->>>>>>> refs/remotes/origin/TrainLine1.0
- *
- */
-/**
- * Test branch 1
- * @author MCF
- *
+ *hahahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
  */
 public class AnnouncementAction extends BaseActionSupport{
 public void test(){
